070377766dfd68b88da111fb163638562b2173e3
Vicente Cheng <vicente.cheng@suse.com>
2023-07-17 15:24:32 +0800

    linter: add golangci configuration
        - also fix the linter warning
        - also bump to go v1.19
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>
    Co-authored-by: Kiefer Chang <kiefer.chang@suse.com>

--- random.go
+++ random.go
@@ -5,7 +5,7 @@ import (
 	"math/big"
 )
 
-// GenRandNumber can generate a random number with a positive boundary value.
+// GenRandNumber generates a random positive number less than the boundary value.
 // The return value should be [0, boundary) and error is nil when success.
 // If the error is not nil, the return value is 0.
 func GenRandNumber(boundary int64) (int64, error) {
