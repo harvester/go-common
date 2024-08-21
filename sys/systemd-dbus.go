198df47889cad910524df842d2604dd51b6cce44
Tim Serong <tserong@suse.com>
2024-08-21 16:23:06 +1000

    feat: Add TryRestartService()
    This is just like the existing RestartService() function, except
    it only applies to services that are already running.
    
    Related issue: https://github.com/harvester/harvester/issues/5274
    
    Signed-off-by: Tim Serong <tserong@suse.com>

--- sys/systemd-dbus.go
+++ sys/systemd-dbus.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// RestartService restarts a service.  If the service isn't already running it will be started.
 func RestartService(unit string) error {
 	ctx := context.Background()
 	conn, err := dbus.NewWithContext(ctx)
@@ -16,6 +17,23 @@ func RestartService(unit string) error {
 	}
 	responseChan := make(chan string, 1)
 	if _, err := conn.RestartUnitContext(ctx, unit, "fail", responseChan); err != nil {
+		logrus.Errorf("Failed to restart service %s. err: %v", unit, err)
+		return err
+	}
+	return nil
+}
+
+// TryRestartService will restart a service, but only if it's currently running.
+// A service that isn't running won't be affected.
+func TryRestartService(unit string) error {
+	ctx := context.Background()
+	conn, err := dbus.NewWithContext(ctx)
+	if err != nil {
+		logrus.Errorf("Failed to create new connection for systemd. err: %v", err)
+		return err
+	}
+	responseChan := make(chan string, 1)
+	if _, err := conn.TryRestartUnitContext(ctx, unit, "fail", responseChan); err != nil {
 		logrus.Errorf("Failed to restart service %s. err: %v", unit, err)
 		return err
 	}
